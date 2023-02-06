eb31b2f52788d4c518690644ef9a91f301db9c80
Ryoma-23 <120545081+Ryoma-23@users.noreply.github.com>
2023-02-06 04:09:02 +0000

    [Add]関数

--- app.js
+++ app.js
@@ -53,15 +53,22 @@
 
 // じゃんけんの手を入力してもらうプロンプト欄を生成
 let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
-
+while ((user_hand != "グー")&&(user_hand != "チョキ")&&(user_hand != "パー")&&(user_hand != null)){
+  alert('グー・チョキ・パーのいずれかを入力してください');
+  user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+}
 // じゃんけんの手をランダムに作成する関数を呼び出す
 let js_hand = getJShand();
 
 // ユーザの手とJavaScriptのじゃんけんの手を比べる関数を呼び出し、結果をjudgeに入れる
 let judge = winLose(user_hand, js_hand);
 
-// 結果を表示する
-alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+if (user_hand != null){
+  alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+  user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+} else {
+  alert("またチャレンジしてね")
+}
 
 // ランダムでじゃんけんの手を作成する関数
 function getJShand(){
@@ -75,7 +82,6 @@ function getJShand(){
   } else if(js_hand_num == 2){
     hand_name = "パー";
   }
-
   return hand_name;
 }
 
